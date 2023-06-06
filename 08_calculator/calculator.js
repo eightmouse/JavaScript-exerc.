ff95a64d67466483c5803e1409b15eae0a0091ba
eightmouse <92474424+eightmouse@users.noreply.github.com>
2023-06-06 16:26:16 +0200

    .

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+  let operator = '+';
+	if (operator == '+') {
+    result = number1 + number2;
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+  let operator = '-';
+  if (operator == '-') {
+    result = number1 - number2;
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let myNums = numbers;
+  let sum = 0;
+  for (let i = 0; i < myNums.length; i++) {
+    sum += myNums[i];
+  }
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  myArray = numbers
+  let myValue = myArray.reduce((a,b)=> a*b);
+  return myValue;
+};
+
+const power = function(myPow) {
+  myPow = Math.pow(4, 3)
+  return myPow;
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+let result = 1;
+if (n == 0 || n == 1) {
+  return result;
+} else {
+  for (let i = n; i >= 1; i--) {
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
