2bd8b798e24794cd1df67a6fb3bca77c588f39d7
eightmouse <92474424+eightmouse@users.noreply.github.com>
2023-06-06 11:31:20 +0200

    .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) / 1.8;
+  let celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * 1.8) + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
