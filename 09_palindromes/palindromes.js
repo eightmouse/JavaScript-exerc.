f8c9dbc571d9f7164bae26c843f72301f01a0409
eightmouse <92474424+eightmouse@users.noreply.github.com>
2023-06-09 11:53:34 +0200

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    for (let i = 0; i < str.lenght / 2; i++) {
+        if (str[i] !== str[str.lenght -i -1]) {
+            return false;
+        } 
+    }
+    return true;
 };
 
 // Do not edit below this line
